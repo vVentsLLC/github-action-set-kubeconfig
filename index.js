fc2396fb08cc1f4e7543fc25bb156a9011262fa4
Aravindan Ve <aravindan.ve@outlook.com>
2020-01-29 02:25:50 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const core = require('@actions/core')
+const io = require('@actions/io')
+
+async function run() {
+  const kubeconfig = core.getInput('kubeconfig')
+
+  core.debug(`kubeconfig input: \n${kubeconfig}}`)
+
+  const fileDir = `/tmp/set_kubeconfig_${Date.now()}`
+  const fileName = 'config'
+  const filePath = path.join(fileDir, fileName)
+
+  core.debug(`writing kubeconfig to: ${filePath}`)
+
+  await io.mkdirP(fileDir)
+  fs.writeFileSync(filePath, kubeconfig, 'utf8')
+
+  core.debug(`Setting KUBECONFIG to: ${filePath}`)
+  core.exportVariable('KUBECONFIG', filePath)
+}
+
+run().catch(error => {
+  console.error(error)
+  core.setFailed(error.message)
+})
